591c424260a78ef2531d8ba0dc7edea004f2a736
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/HowToAddCustomControl/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Sample.WebForm1" %>
+
+<%@ Register Assembly="DevExpress.XtraReports.v16.1.Web, Version=16.1.1.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.XtraReports.Web" TagPrefix="dx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<head runat="server">
+    <title></title>
+
+    <link href="CustomLabel/custom-label.css" rel="stylesheet" />
+    <!-- #include file = "CustomLabel\custom-label-content.html" -->
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <dx:ASPxReportDesigner ID="reportDesigner1" runat="server">
+        </dx:ASPxReportDesigner>
+
+        <script src="CustomLabel/custom-label.js"></script>
+        <script type="text/javascript">
+
+            DevExpress.Designer.Report.controlsFactory.registerControl("<%= typeof(Sample.CustomLabel).ToString() %>", {
+                info: newSerializationsInfo,
+                defaultVal: {
+                    "@ControlType": "<%= typeof(Sample.CustomLabel).AssemblyQualifiedName %>",
+                    "@SizeF": "100,23"
+                },
+                type: DevExpress.Designer.Report.ControlViewModel,
+                surfaceType: CustomLabelSurface,
+                popularProperties: newPopularProperties,
+                elementActionsTypes: [DevExpress.Designer.ElementActions]
+            });
+        </script>
+    </form>
+</body>
+</html>
---
+++ VB/HowToAddCustomControl/My Project/AssemblyInfo.vb
@@ -0,0 +1,61 @@
+﻿' Developer Express Code Central Example:
+' ASPxReportDesigner - How to register a custom control in Report Designer's toolbox
+' 
+' This example shows how to create a custom control and register it in the Web
+' Report Designer
+' (https://documentation.devexpress.com/#XtraReports/CustomDocument17103)'s
+' Toolbox (https://documentation.devexpress.com/#XtraReports/CustomDocument17559).
+' The example illustrates the steps to expand a set of native report controls by
+' deriving from existing ones.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=T209289
+
+' Developer Express Code Central Example:
+' How to: Add a Custom Control in the Web Report Designer
+' 
+' This example shows how to create a custom control and register it in the Web
+' Report Designer
+' (https://documentation.devexpress.com/#XtraReports/CustomDocument17103)'s
+' Toolbox (https://documentation.devexpress.com/#XtraReports/CustomDocument17559).
+' The example illustrates the steps to expand a set of native report controls by
+' deriving from existing ones.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=T209289
+
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("Sample")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Sample")>
+<Assembly: AssemblyCopyright("Copyright ? 2015")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3a735021-8971-44a6-a663-7cf14e285c37")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Revision and Build Numbers 
+' by using the '*' as shown below:
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
