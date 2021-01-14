dfb7d634bb9a7c238a60c431a2365558b72ad0f1
BrianDX <boris.zaitsev@devexpress.com>
2021-01-14 08:43:47 +0300

    Update Designer.aspx.cs

--- CS/CustomReportControlSample/Designer.aspx.cs
+++ CS/CustomReportControlSample/Designer.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 namespace CustomReportControlSample {
     public partial class Designer : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-            ASPxReportDesigner1.OpenReport("CustomReportControl");
+            //ASPxReportDesigner1.OpenReport("CustomReportControl");
         }
     }
 }
