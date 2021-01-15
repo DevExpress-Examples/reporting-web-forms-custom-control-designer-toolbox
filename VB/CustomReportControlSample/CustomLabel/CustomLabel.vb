bb1cddcbf9fe88b4b0539d482c4bea94ec311be9
DevExpressExampleBot <fakeEmail>
2021-01-15 14:18:43 +0300

    Source auto update [skip ci]

---
+++ VB/CustomReportControlSample/CustomLabel/CustomLabel.vb
@@ -0,0 +1,26 @@
+﻿Imports System.ComponentModel
+Imports DevExpress.XtraReports.UI
+Imports DevExpress.Utils.Serializing
+
+Namespace CustomReportControlSample
+	Public Class CustomLabel
+		Inherits XRLabel
+
+		<Browsable(True), Bindable(False), Category("Data")>
+		<XtraSerializableProperty>
+		<DefaultValue("")>
+		<DesignerSerializationVisibility(DesignerSerializationVisibility.Visible), EditorBrowsable(EditorBrowsableState.Always)>
+		Public Property CustomNumber() As String
+
+		<Browsable(True), Bindable(True), Category("Data")>
+		<XtraSerializableProperty>
+		<DefaultValue("")>
+		<DesignerSerializationVisibility(DesignerSerializationVisibility.Visible), EditorBrowsable(EditorBrowsableState.Always)>
+		Public Property StringData() As String
+
+		Protected Overrides Sub PutStateToBrick(ByVal brick As DevExpress.XtraPrinting.VisualBrick, ByVal ps As DevExpress.XtraPrinting.PrintingSystemBase)
+			MyBase.PutStateToBrick(brick, ps)
+			brick.Text = If(StringData, CustomNumber)
+		End Sub
+	End Class
+End Namespace
---
+++ VB/CustomReportControlSample/PredefinedReports/ReportsFactory.vb
@@ -0,0 +1,14 @@
+﻿Imports DevExpress.XtraReports.UI
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Threading.Tasks
+
+Namespace CustomReportControlSample.PredefinedReports
+	Public Module ReportsFactory
+		Sub New()
+			Reports.Add("TestReport", Function() New TestReport())
+		End Sub
+		Public Reports As New Dictionary(Of String, Func(Of XtraReport))()
+	End Module
+End Namespace
